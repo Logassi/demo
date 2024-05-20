449be513b0591ae5cff7772b671bd41e0401ef86
Logassi <lomonagagaspas@gmail.com>
2024-05-20 21:18:25 +0700

    Creating Student's Business Layer; (StudentService), and Dependency Injection; [@Service/@Component]

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,17 +12,15 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
     @GetMapping
     public List<Student> getStudent(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Anomol Isagag",
-                        "anomolisagag@gmail.com",
-                        LocalDate.of(2000, Month.APRIL,3),
-                        23
-                )
-        );
+        return studentService.getStudent();
     }
 
 }
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,25 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudent(){
+        return List.of(
+                new Student(
+                        1L,
+                        "Anomol Isagag",
+                        "anomolisagag@gmail.com",
+                        LocalDate.of(2000, Month.APRIL,3),
+                        23
+                )
+        );
+    }
+
+}
