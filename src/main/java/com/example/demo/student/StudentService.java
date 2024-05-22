b13bc67f226f1eb2c8c82402ffbad3c35d0ab375
Logassi <lomonagagaspas@gmail.com>
2024-05-22 13:03:02 +0700

    Creating Post Mapping, JPQL, and Trying Postman

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,4 +22,14 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.
+                findStudentByEmail(student.getEmail());
+
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("Email Taken");
+        }
+
+        studentRepository.save(student);
+    }
 }
